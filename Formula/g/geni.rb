ae803b5eb22688188d9d3b2b1b2923f52afd6590
Emil Priver <emil@priver.dev>
2024-03-21 11:36:03 +0100

    Update geni.rb

--- Formula/g/geni.rb
+++ Formula/g/geni.rb
@@ -2,7 +2,7 @@ class Geni < Formula
   desc "Database CLI migration tool"
   homepage "https://github.com/emilpriver/geni"
   url "https://github.com/emilpriver/geni/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "bf1443035c3cce97c594363650e53f16513f39aa0f69efc9152501f9529b79ab"
   license "MIT"
 
   depends_on "rust" => :build
